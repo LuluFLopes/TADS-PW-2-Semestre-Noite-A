64ad7223b9f0d9d6206455f4745c67b0a180fdc8
Henry Felipe <henrycalleraa@gmail.com>
2022-06-08 22:16:47 -0300

    tela logiin

--- Adimo/public_html/js/login.js
+++ Adimo/public_html/js/login.js
@@ -4,16 +4,22 @@ let emailFormat = document.querySelector("#info-email");
 let botaoConfirmar = document.querySelector("#botaoConfirmar");
 
 botaoConfirmar.addEventListener("click", function (event) {
-    event.preventDefault();
+    try {
+        event.preventDefault();
 
-    if (emailFormat.value == "senac@senac.com.br" && senhaFormat.value == "1") {
-
-        let a = document.getElementById("mudanca-tela");
-
-        a.href = "index.html";
-
-        window.open("index.html");
-        window.close("login.html");
-
+        if (emailFormat.value == "senac@senac.com.br" && senhaFormat.value == "1") {
+    
+            let a = document.getElementById("mudanca-tela");
+    
+            a.href = "index.html";
+    
+            window.open("index.html");
+            window.close("login.html");
+    
+        }else{
+            alert('Login incorreto')
+        }
+    } catch (error) {
+        alert(error)
     }
 });
