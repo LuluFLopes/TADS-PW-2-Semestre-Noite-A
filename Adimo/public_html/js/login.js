93dcc151867a304dd7f3138db265034e6d0ea3fd
Luciano <luc.merce@hotmail.com>
2022-06-08 22:05:29 -0300

    Ligação das telas e criação da tela de login.js

---
+++ Adimo/public_html/js/login.js
@@ -0,0 +1,19 @@
+let senhaFormat = document.querySelector("#info-senha");
+let emailFormat = document.querySelector("#info-email");
+
+let botaoConfirmar = document.querySelector("#botaoConfirmar");
+
+botaoConfirmar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (emailFormat.value == "senac@senac.com.br" && senhaFormat.value == "1") {
+
+        let a = document.getElementById("mudanca-tela");
+
+        a.href = "index.html";
+
+        window.open("index.html");
+        window.close("login.html");
+
+    }
+});
